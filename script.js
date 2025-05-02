ccced0abe20aefb03cf36b98395f74c44285265d
C0D3RW0LF <85829513+c0d3rw0lf@users.noreply.github.com>
2025-05-02 12:06:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const animatedName = document.getElementById("animated-name");
 const themeChanger = document.getElementById("theme-changer");
 
-const tags = ["Zakaria", "C0D3RW0LF", "K4RM4W0LF", "Zacker010011"];
+const tags = ["Welcome, Seeker"];
 let tagIdx = 0;
 
 function sleep(ms) {
@@ -9,7 +9,7 @@ function sleep(ms) {
 }
 
 async function typerAnimation(text) {
-    animatedName.textContent = "Hi, I am "
+    animatedName.textContent = ""
     for (let i = 0; i < text.length; i++) {
         animatedName.textContent += text.charAt(i);
         await sleep(100);
